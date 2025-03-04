3007ddf3429583aacb5a2af4a913aa02359eeef7
Dibou960 <Dibou960@gmail.com>
2025-03-04 03:42:40 +0000

    Ajout du script d'installation de FFmpeg

--- .render-build.sh
+++ .render-build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-curl -sL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ && \
-    mv ffmpeg-*-static/ffmpeg /usr/local/bin/ && \
-    mv ffmpeg-*-static/ffprobe /usr/local/bin/
-chmod +x .render-build.sh
+
+# Télécharger FFmpeg
+curl -sL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ 
+
+# Définir le chemin vers FFmpeg (sans déplacement)
+export PATH="$PWD/ffmpeg-*-static:$PATH"
+
+# Vérifier si FFmpeg fonctionne
+ffmpeg -version
